6f5a07e3008917b2734aacd57d5518152b541117
surajit <surajit.greymirror@gmail.com>
2025-01-14 12:42:46 +0530

    little much done , but still issue on particia tree

--- src/counter.ts
+++ src/counter.ts
@@ -23,26 +23,30 @@ class Trie {
   }
   add(word: string, root = this.root, sublnt = 0) {
     let current = root || this.root;
-    console.log("word,root.", word, current);
+    const isLeaf = current.children.size === 0;
+    if (word == "pplication") {
+      console.log("current :>> ", current);
+    }
 
+    let isMatched = false;
+    // if current root has children the sent the uncommon string to childs
     current.children.forEach((value, key) => {
       if (word.startsWith(key)) {
+        isMatched = true;
         const commonIndex = this.getCommonPrefixLength(key, word);
-        console.log("commonIndex", commonIndex);
         const childWord = word.slice(commonIndex);
         this.add(childWord, value, ++sublnt);
         return;
       }
     });
 
+    //initial case where sublnt is 0
     if (sublnt === 0) {
-      //initial case
-      console.log("init");
       const initNode = new PatriciaNode(word[0]);
       current.children.set(word[0], initNode);
       this.add(word.slice(1), initNode, ++sublnt);
-    } else if (current.children.size === 0) {
-      console.log("orphane");
+    } else if ((isLeaf && word) || !isMatched) {
+      // this case refer the current root has no child to compare further so add in the list
       const children = new PatriciaNode(word);
       children.eow = true;
       current.children.set(word, children);
@@ -58,24 +62,37 @@ class Trie {
   }
 }
 const t = new Trie();
-t.add("pen");
-t.add("pens");
-const words: string[] = [];
-let w = "";
-getChilds(t.root);
+t.add("app");
+t.add("apple");
+t.add("api");
+t.add("application");
+// t.add("applause");
+// t.add("appetite");
+// t.add("triple");
+// t.add("trap");
+// t.add("apple");
+// t.add("appraise");
+// t.add("apparatus");
+// t.add("appendix");
+// t.add("apprentice");
+// t.add("appoint");
+// t.add("mumbai");
+// t.add("bottle");
+// t.add("dsa");
+// t.add("surajit");
+// t.add("javascript");
+// t.add("god");
+// t.add("pens");
 
-function getChilds(root: TPatriciaTrieNode) {
-  root.children.forEach((value, key) => {
-    console.log("value.eow,value.prefix", value.eow, value.prefix);
-    if (value.eow) {
-      w += value.prefix;
-      words.push(w);
-      getChilds(value);
-    } else {
-      w += value.prefix;
-      getChilds(value);
-    }
+function getChilds(root: TPatriciaTrieNode, pStr = "") {
+  if (root.eow) {
+    console.log("object :>> ", pStr + root.prefix);
+  }
+  if (root.children.size === 0) return;
+  root.children.forEach((value) => {
+    getChilds(value, pStr + root.prefix);
   });
 }
-console.log("words", words);
+
+getChilds(t.root);
 export default t;
